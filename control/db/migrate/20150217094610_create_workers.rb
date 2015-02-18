52416de1b1b1728d16075255f62284014240b739
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-02-18 10:34:04 +0200

    Added not null constraints to the migrations

--- control/db/migrate/20150217094610_create_workers.rb
+++ control/db/migrate/20150217094610_create_workers.rb
@@ -1,8 +1,8 @@
 class CreateWorkers < ActiveRecord::Migration
   def change
     create_table :workers do |t|
-      t.string :title
-      t.string :address
+      t.string :title, :null => false
+      t.string :address, :null => false
 
       t.timestamps null: false
     end
--- control/db/schema.rb
+++ control/db/schema.rb
@@ -26,15 +26,15 @@ ActiveRecord::Schema.define(version: 20150217150321) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "workers", force: :cascade do |t|
-    t.string   "title"
-    t.string   "address"
+    t.string   "title",      null: false
+    t.string   "address",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
