1859bbfa4f6f0cb4d74ff0310136850526095615
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-03-01 11:32:20 +0200

    Implemented few models tests

--- control/spec/models/base_version_spec.rb
+++ control/spec/models/base_version_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe BaseVersion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with name' do
+    base_version = BaseVersion.new(:name => "3.0")
+    expect(base_version).to be_valid
+  end
+  
+  it 'is invalid without name' do
+    base_version = BaseVersion.new(:name => nil)
+    expect(base_version).to be_invalid
+  end
+  
+  it 'is invalid with to short name' do
+    base_version = BaseVersion.new(:name => "")
+    expect(base_version).to be_invalid
+  end
+  
+  it 'is invalid with to long name' do
+    base_version = BaseVersion.new(:name => "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789")
+    expect(base_version).to be_invalid
+  end
+  
+  it 'is invalid with duplicate name' do
+    BaseVersion.create(:name => "4.1")
+    base_version = BaseVersion.new(:name => "4.1")
+    expect(base_version).to be_invalid
+  end
+  
 end
--- control/spec/models/branch_spec.rb
+++ control/spec/models/branch_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Branch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'filters all branches' do
+    names = ["master", "release_36", "release_37", "release_38_tests", "12345_feature_34", "12345_feature_35", 
+      "57845_feature_40", "master1", "masterhjgd", "124master", "master_tesing"]
+    names.each do |name|
+      Branch.create(:name => name)
+    end
+    expect(Branch.all_filtered("").to_a.map {|b| b.name}).to match_array names
+  end
+  
+  it 'filters branches with regex' do
+    names = ["master", "release_36", "release_37", "release_38_tests", "12345_feature_34", "12345_feature_35", 
+      "57845_feature_40", "master1", "masterhjgd", "124master", "master_tesing"]
+    names.each do |name|
+      Branch.create(:name => name)
+    end
+    expect(Branch.all_filtered(/(release|master)/).to_a.map {|b| b.name}).to match_array ["master", "release_36", 
+      "release_37", "release_38_tests", "master1", "masterhjgd", "124master", "master_tesing"]
+  end
+  
 end
