8ffa216dba8c959f67af4c9b6f58721971e0c20a
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-03-01 09:24:50 +0200

    Started cleanup

--- control/spec/controllers/base_versions_controller_spec.rb
+++ control/spec/controllers/base_versions_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe BaseVersionsController, type: :controller do
   # BaseVersion. As you add validations to BaseVersion, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {name: "hello"}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {name: ""}
   }
 
   # This should return the minimal set of values that should be in the session
