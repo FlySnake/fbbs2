5e181ecbf3355b3a728203325ca3d596c75c487b
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-03-17 08:58:47 +0200

    Fix migration and build numbers index error (issue #13)

--- control/db/migrate/20150317035242_remove_foreign_key_to_notify_user_from_build_job.rb
+++ control/db/migrate/20150317035242_remove_foreign_key_to_notify_user_from_build_job.rb
@@ -1,6 +1,6 @@
 class RemoveForeignKeyToNotifyUserFromBuildJob < ActiveRecord::Migration
   def up
-    remove_foreign_key :build_jobs, :notify_user_id
+    remove_foreign_key :build_jobs, :notify_user
     change_column_null :build_jobs, :notify_user_id, true
   end
   
