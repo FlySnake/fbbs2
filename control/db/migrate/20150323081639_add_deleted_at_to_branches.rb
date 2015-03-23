0e95e2e3713bab5251e9a8282b915e3e7fd42b04
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-03-23 16:58:43 +0200

    Fix migration

--- control/db/migrate/20150323081639_add_deleted_at_to_branches.rb
+++ control/db/migrate/20150323081639_add_deleted_at_to_branches.rb
@@ -3,12 +3,10 @@ class AddDeletedAtToBranches < ActiveRecord::Migration
     add_column :branches, :deleted_at, :datetime
     add_index :branches, :deleted_at
     remove_column :branches, :deleted
-    add_index :branches, :name
   end
   
   def down
     remove_column :branches, :deleted_at
     add_column :branches, :deleted, :boolean, default: false, null: false
-    remove_index :branches, :name
   end
 end
