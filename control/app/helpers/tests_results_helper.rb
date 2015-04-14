070ef49a6f264319f52f073d68749700b4fe5b56
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2015-04-14 16:29:23 +0200

    Visualization finally implemented (no tests)

--- control/app/helpers/tests_results_helper.rb
+++ control/app/helpers/tests_results_helper.rb
@@ -1,2 +1,10 @@
 module TestsResultsHelper
+  def result_text tcase
+    if tcase.result == :ok
+      res = "<font color='green'>ok</font>"
+    else
+      res = "<font color='red'>fail</font><pre>#{tcase.failure_message}</pre>"
+    end
+    res.html_safe
+  end
 end
--- control/app/models/build_job.rb
+++ control/app/models/build_job.rb
@@ -165,7 +165,7 @@ class BuildJob < ActiveRecord::Base
         file.flush
         if self.run_tests and not self.enviroment.tests_executor.nil?
           if self.enviroment.tests_executor.artefact_name == artefact.filename #this is a tests result
-            TestsResult.process_artefact file
+            TestsResult.process_artefact file.path, self, self.enviroment.tests_executor
           end
         else
           artefact.file = file
